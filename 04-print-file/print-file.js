7aa455b6ac7b71d11093ebd55cab9130673c1f78
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-01 15:34:02 +0530

    reading writing files

--- 04-print-file/print-file.js
+++ 04-print-file/print-file.js
@@ -1,6 +1,66 @@
 var fs = require('fs');
 
-var fileContents = fs.readFileSync('./sample.txt', { encoding : 'utf8'});
+/* sync */
 
-console.log(fileContents);
-console.log('thats all folks!');
+/* try{
+    var fileContents = fs.readFileSync('./sample1.txt', { encoding : 'utf8'});
+    console.log(fileContents);
+    console.log('thats all folks!'); 
+} catch(err){
+    console.log('something went wrong!');
+    console.log(err);
+}
+ */
+
+
+/* async */
+// error first callback pattern
+
+/*  fs.readFile('./sample.txt', { encoding: 'utf8' }, function(err, fileContents){
+    if (err){
+        console.log('something went wrong!');
+        console.log(err);
+        return;
+    }
+    console.log(fileContents);
+    console.log('thats all folks!'); 
+}); */
+
+/* streams */
+
+/* 
+var stream = fs.createReadStream('./sample.txt', { encoding : 'utf8'});
+//readable stream events - open, data, end, close, error
+
+var counter = 0;
+stream.on('data', function(chunk){
+    console.log(chunk);
+    ++counter;
+});
+
+stream.on('end', function(){
+    console.log('thats all folks!'); 
+    console.log('read completed with ', counter, ' chunks');
+});
+
+stream.on('error', function(err){
+    console.log('something went wrong!');
+    console.log(err);
+});
+ */
+
+/* 
+Abstractions
+1. ReadStream (Stream.Readable), WriteStream (Stream.Writable)
+2. EventEmitter (events)
+*/
+
+var stream = fs.createReadStream('./sample.txt', { encoding : 'utf8'});
+/* var dest = fs.createWriteStream('./newSample.txt');
+stream.pipe(dest); */
+stream.pipe(process.stdout);
+
+stream.on('end', function(){
+    console.log('thats all folks!');
+});
+
