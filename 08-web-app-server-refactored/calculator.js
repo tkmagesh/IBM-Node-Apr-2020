217ab331dcd15e9d6f8b43c650d46349d5b71e91
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-02 15:00:27 +0530

    implementing data parser

---
+++ 08-web-app-server-refactored/calculator.js
@@ -0,0 +1,16 @@
+var calculator = {
+    add : function (x,y){
+        return x + y;
+    },
+    subtract : function(x,y){
+        return x - y;
+    },
+    multiply(x,y){
+        return x * y;
+    },
+    divide(x,y){
+        return x / y;
+    },
+}
+
+module.exports = calculator;
