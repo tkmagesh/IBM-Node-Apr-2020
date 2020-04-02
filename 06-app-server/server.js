71566c551b86dab3502ef0ce8533c839140f5e94
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-02 12:49:02 +0530

    web app server - assignment

--- 06-app-server/server.js
+++ 06-app-server/server.js
@@ -1,11 +1,11 @@
 var http = require('http'),
     url = require('url'),
     querystring = require('querystring'),
-    calculator = require('./calculator');
+    calculator = require('./calculator'); 
 
 var server = http.createServer(function(req, res){
     var urlObj = url.parse(req.url);
-    if (urlObj.pathname === '/calculator'){
+    if (urlObj.pathname === '/calculator' && req.method === 'GET'){
         var data = querystring.parse(urlObj.query),
             op = data.op,
             x = parseInt(data.x),
@@ -13,6 +13,20 @@ var server = http.createServer(function(req, res){
         var result = calculator[op](x,y);
         res.write(result.toString());
         res.end();
+    } if (urlObj.pathname === '/calculator' && req.method === 'POST') {
+        var rawData = '';
+        req.on('data', function(chunk){
+            rawData += chunk;
+        });
+        req.on('end', function(){
+            var data = querystring.parse(rawData),
+                op = data.op,
+                x = parseInt(data.x),
+                y = parseInt(data.y);
+            var result = calculator[op](x, y);
+            res.write(result.toString());
+            res.end();
+        });
     } else {
         res.statusCode = 404;
         res.end();
---
+++ 07-web-app-server/server.js
@@ -0,0 +1,1 @@
+//create the server that will behave both like a web server and app server depending on the request
