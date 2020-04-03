7e40d8de88097622ef79031b09c21ab2502a7fde
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-03 10:29:37 +0530

    Update bugRoutes.js

--- 10-bug-tracker/routes/bugRoutes.js
+++ 10-bug-tracker/routes/bugRoutes.js
@@ -31,5 +31,28 @@ router.get('/:id', function(req, res, next){
     } else {
         res.sendStatus(404);
     }
-})
+});
+
+router.post('/', function(req, res, next){
+    var newBugData = req.body;
+    //update the array
+    //return the newbug with status code 201
+});
+
+router.put('/:id', function (req, res, next) {
+    var bugId = parseInt(req.params.id),
+        updatedBugData = req.body;
+    //update the bugsList array
+    //return the updated bug
+    //return 404 if the bug does not exist
+});
+
+router.delete('/:id', function (req, res, next) {
+    var bugId = parseInt(req.params.id),
+       
+    //delte the bug from the array
+    //return 200
+    //return 404 if the bug does not exist
+});
+
 module.exports = router;
