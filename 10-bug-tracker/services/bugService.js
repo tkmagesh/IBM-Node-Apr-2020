d8167d6e95a2996a349b863db263f092bb129d6d
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-03 12:26:49 +0530

    persisting in db

--- 10-bug-tracker/services/bugService.js
+++ 10-bug-tracker/services/bugService.js
@@ -1,37 +1,31 @@
-var bugsList = [
-    {
-        id: 1,
-        name: 'Server communication failure',
-        isClosed: false,
-        createdAt: new Date()
-    },
-    {
-        id: 2,
-        name: 'Application not responding',
-        isClosed: true,
-        createdAt: new Date()
-    },
-];
+//modify the below code to persist the data in the db/bugDb.json file
+
+//use JSON.stringify and JSON.parse
 
 function getAll(){
+    //read the db
     return bugsList;
 }
 
 function get(id){
+    //read the db
     return bugsList.find(function(bug){
         return bug.id === id;
     });
 }
 
 function addNew(bug){
+        //read the db
         bug.id  = bugsList.reduce(function (result, bug) {
             return result > bug.id ? result : bug.id;
         }) + 1;
         bugsList.push(bug);
+        //write to the db
         return bug;
 }
 
 function update(id, updatedBug){
+    //read the db
     var existingBug = bugsList.find(function (bug) {
         return bug.id === id;
     });
@@ -39,10 +33,12 @@ function update(id, updatedBug){
     bugsList = bugsList.map(function (bug) {
         return bug.id === id ? updatedBug : bug;
     });
+    //write to the db
     return updatedBug;
 }
 
 function remove(bugId){
+    //read the db
     var existingBug = bugsList.find(function (bug) {
         return bug.id === bugId;
     }); 
@@ -50,6 +46,7 @@ function remove(bugId){
     bugsList = bugsList.filter(function (bug) {
         return bug.id !== bugId;
     });
+    //write to the db
     return true;
 }
 
